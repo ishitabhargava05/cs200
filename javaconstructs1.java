7f3bf7a1e8f36706a41eef5de4fcfa7f2a31179b
ishitabhargava05 <ib115@wellesley.edu>
2024-07-08 03:16:20 -0400

    Add files via upload

---
+++ javaconstructs1.java
@@ -0,0 +1,119 @@
+
+/**
+ * Java Constructs - 1 (a)
+ *
+ * @author Ishita Bhargava 
+ * @version 07/08/2024
+ */
+public class javaconstructs1
+{
+    /**
+     * Q1. We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. 
+     *     We are in trouble if they are both smiling or if neither of them is smiling. 
+     * @param aSmile, true if monkey a is smiling.
+     * @param bSmile, true if monkey b is smiling. 
+     * @return true, if we are in trouble. 
+     */
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile){
+        return false;
+    }
+    
+    /**
+     * Q2. Given two int values, return whichever value is larger.
+     *     However if the two values have the same remainder when divided by 5, then the return the smaller value.
+     *     However, in all cases, if the two values are the same, return 0. 
+     * @param a, b integers to be compared
+     * @return 
+     */
+    public int maxMod5(int a, int b){
+        return 0;
+    }
+    
+    /**
+     * Q3. When squirrels get together for a party, they like to have cigars.
+     *     A squirrel party is successful when the number of cigars is between 45 and 65, inclusive. 
+     *     Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+     *     Return true if the party with the given values is successful, or false otherwise. 
+     *     @param
+     *     @param
+     *     @return
+     */
+    public boolean cigarParty(int numOfCigars, boolean weekend){
+        return false;
+    }
+    
+    /**
+     * Q4. Write a program to find the factorial value of any number. 
+     * @param
+     * @return
+     */
+    public int factorial(int num){
+        return 0;
+    }
+    
+    /**
+     * Q5. Return true if the given non-negative number is 1 or 2 less than a multiple of 20.
+     * @param 
+     * @return 
+     */
+    public boolean less20(int num){
+        return false;
+    }
+    
+    /**
+     * Q6. Given three ints, a b c, return true if b is greater than a, and c is greater than b. 
+     *     However, with the exception that if "bOk" is true, b does not need to be greater than a.
+     * @param
+     * @return 
+     */
+    public boolean inOrder(int a, int b, int c, boolean bOk){
+        return false;
+    }
+    
+    /**
+     * Q7. Write a program called sumOfSeven to sum those numbers from 1 to 100 that are divisible by 7. 
+     *     Display the sum and the average.  
+     * @return
+     */
+    public void sumOfSeven(){
+        
+    }
+    
+    /**
+     * Q8. You and your date are trying to get a table at a restaurant. 
+     *     The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes.
+     *     The result of getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. 
+     *     If either of you is very stylish, 8 or more, then the result is 2 (yes). 
+     *     With the exception that if either of you has style of 2 or less, then the result is 0 (no). 
+     *     Otherwise the result is 1 (maybe).
+     * @param
+     * @return
+     */
+    public int dateFashion(int you, int date){
+        return 0;
+    }
+    
+    /**
+     * Q9. We want to make a package of goal kilos of chocolate. 
+     *     We have small bars (1 kilo each) and big bars (5 kilos each). 
+     *     Return the number of small bars to use, assuming we always use big bars before small bars.
+     *     Return -1 if it can't be done.
+     * @param
+     * @param
+     * @return
+     */
+    public int makeChocolate(int small, int big, int goal){
+        return 0;
+    }
+    
+    /**
+     * Q10. Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. 
+     *      Return 0 if they both go over.
+     * @param
+     * @param
+     * @return 
+     */
+    public int blackjack(int num1, int num2){
+        return 0;
+    }
+}
